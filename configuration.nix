a45b39c6c37636df5df59211148cecbbcd174f7b
gusjengis <anthony.j.green@outlook.com>
2025-03-04 14:34:07 -0800

    adding steam and waybar for fun

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -55,24 +56,24 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-   # services.xserver.displayManager.gdm.enable = true;
-   # services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
-# Enable Hyprland <
-services.xserver.displayManager.gdm.wayland = true;  
+  # Enable Hyprland <
+  services.xserver.displayManager.gdm.wayland = true;
 
-programs.hyprland = {    
-    enable = true;    
-    xwayland.enable = true;    
-}; 
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
-programs.nix-ld.enable = true;
-programs.nix-ld.libraries = with pkgs; [
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
 
-];
-programs.bash.shellAliases = {
-  nvim = "sudo -E -s nvim";
-};
+  ];
+  programs.bash.shellAliases = {
+    nvim = "sudo -E -s nvim";
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -108,7 +109,7 @@ programs.bash.shellAliases = {
     description = "Anthony Green";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -134,19 +135,20 @@ programs.bash.shellAliases = {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  	git
-  	neovim
-	kitty
-	lazygit
-	rustup
-	gcc
-	cargo
-	wl-clipboard
-	gh
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    git
+    neovim
+    kitty
+    lazygit
+    rustup
+    gcc
+    cargo
+    wl-clipboard
+    gh
+    steam
+    waybar
   ];
-
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
