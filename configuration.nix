3d8f41f6cc9bd81a4f00685c06a922c75df833aa
gusjengis <anthony.j.green@outlook.com>
2025-03-05 07:57:19 -0800

    added some nvim dependencies, trying to enable waybar

--- configuration.nix
+++ configuration.nix
@@ -148,7 +148,15 @@
     gh
     steam
     waybar
+    unzip
+    # make
+    wget
+    fzf
   ];
+
+  programs.waybar = {
+    enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
