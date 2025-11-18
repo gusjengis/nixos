b9651a83eb71c76cce1916ff14d614ef29b38594
gusjengis <anthony.j.green@outlook.com>
2025-11-17 18:55:47 -0800

    removed cutting edge hyprland source from flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     stable-nixpkgs.url = "nixpkgs/nixos-25.05";
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
   outputs =
