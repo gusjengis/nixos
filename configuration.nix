31915f96cd918968fb1e7066f7710d124fd24968
gusjengis <anthony.j.green@outlook.com>
2026-02-02 14:22:57 -0800

    added vm stuff

--- configuration.nix
+++ configuration.nix
@@ -7,5 +7,23 @@
   ...
 }:
 {
+  # VM stuff for testing AUR packages
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    qemu_kvm
+    virt-manager
+    virt-viewer
+    libvirt
+    OVMF
+    spice-gtk
+  ];
+
+  users.users.gusjengis.extraGroups = [
+    "libvirtd"
+    "kvm"
+  ];
+
   system.stateVersion = "25.05";
 }
