765ef592a44fed478fb4136997f7bac3718dbf35
gusjengis <anthony.j.green@outlook.com>
2026-02-17 10:16:30 -0800

    t

--- configuration.nix
+++ configuration.nix
@@ -7,23 +7,9 @@
   ...
 }:
 {
-  # VM stuff for testing AUR packages
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    qemu_kvm
-    virt-manager
-    virt-viewer
-    libvirt
-    OVMF
-    spice-gtk
-  ];
-
-  users.users.gusjengis.extraGroups = [
-    "libvirtd"
-    "kvm"
-  ];
+  steam.enable = true;
+  nvidia.enable = true;
+  virtual-machines.enable = true;
 
   system.stateVersion = "25.05";
 }
