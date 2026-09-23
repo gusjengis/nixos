5d279ced4e96e905d33c22422a1bfcf1b21b38eb
gusjengis <anthony.j.green@outlook.com>
2026-09-22 23:18:51 -0700

    more reliable wifi on main pc

--- system/hosts/pc/configuration.nix
+++ system/hosts/pc/configuration.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 
@@ -8,8 +9,12 @@
   ];
 
   bedtimeLockout.enable = false;
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.networkmanager.wifi.powersave = false;
   nvidia.enable = true;
   virtual-machines.enable = true;
   programs.steam.enable = true;
   system.stateVersion = "25.05";
+
+  environment.systemPackages = [ pkgs.iwd ];
 }
